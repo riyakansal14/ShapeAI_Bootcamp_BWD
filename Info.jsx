f2981bfecf7a8c9a9b53ff22122298c48ecedd7d
Riya Kansal <78086124+riyakansal14@users.noreply.github.com>
2021-06-28 09:59:05 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Conversation Shuffle</h1>
+      <p>Hosted by Riya on 30th June' 21 at 6 pm for Spanish beginners</p>
+    </div>
+  );
+}
+
+export default Info;
